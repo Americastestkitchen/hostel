47892daa6bf511e4cca78f35438b5959d0ff2b6d
Nathan Lilienthal <nathan@nixpulvis.com>
2014-05-13 15:16:41 -0400

    JSON fixes?

--- lib/hostel/site.rb
+++ lib/hostel/site.rb
@@ -4,10 +4,6 @@ require 'active_support/core_ext/module/delegation'
 
 module Hostel
   class Site < OpenStruct
-    # OpenStructs don't convert to json very well,
-    # so we convert to hash first
-    delegate :to_json, :as_json, to: :to_h
-
     def initialize(*args)
       super
       self.class.delegate "#{self.key}?", to: :inquirable_key
@@ -21,10 +17,9 @@ module Hostel
       ActiveSupport::StringInquirer.new(self.key.to_s)
     end
 
-    def to_json
-      data = self.as_json
-      data[:domain] = domain
-      data.to_json
+    def as_json(options = nil)
+      self[:domain] = domain
+      super(options)
     end
   end
 end
