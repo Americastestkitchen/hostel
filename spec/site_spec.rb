503acafd231ed327424362ba2d26b13047048e59
Matthew King <matthew.king@americastestkitchen.com>
2014-10-06 16:22:28 -0400

    Adding build_path method to Hostel::Site

--- spec/site_spec.rb
+++ spec/site_spec.rb
@@ -17,3 +17,25 @@ describe Hostel::Site, 'key delegation' do
     site.other?.should == false
   end
 end
+
+describe Hostel::Site, '#build_path' do
+  it 'returns secure url with path with one arg' do
+    site = Hostel::Site.new(key: 'test', subdomain: 'www-test', domain_without_subdomain: 'americastestkitchen.com')
+    site.build_path('order').should == 'https://www-test.americastestkitchen.com/order'
+  end
+
+  it 'returns secure url with path with one arg with leading slash' do
+    site = Hostel::Site.new(key: 'test', subdomain: 'www-test', domain_without_subdomain: 'americastestkitchen.com')
+    site.build_path('/order').should == 'https://www-test.americastestkitchen.com/order'
+  end
+
+  it 'returns insecure url with secure set to false' do
+    site = Hostel::Site.new(key: 'test', subdomain: 'www-test', domain_without_subdomain: 'americastestkitchen.com')
+    site.build_path('/order', secure: false).should == 'http://www-test.americastestkitchen.com/order'
+  end
+
+  it 'returns secure url with params when params are set' do
+    site = Hostel::Site.new(key: 'test', subdomain: 'www-test', domain_without_subdomain: 'americastestkitchen.com')
+    site.build_path('order', path_args: { :incode => 'ATKSTUFF', "extcode" => "CIOTHINGS" }).should == 'https://www-test.americastestkitchen.com/order?incode=ATKSTUFF&extcode=CIOTHINGS'
+  end
+end
